92d82764db3bf8e7498b749ddd3262b5c67bb610
“Andrew <andrewwoods88@gmail.com>
2018-03-19 13:22:04 -0400

    Wired up the route for PostsIndex

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsIndex extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Post Index</h2>
+            </div>
+        );
+    }
+}
+
+export default PostsIndex;
